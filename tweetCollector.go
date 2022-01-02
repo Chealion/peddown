ff8a943adbcac4231c531269994202a1170f1cd3
Micheal Jones <micheal@mcjones.ca>
2022-01-02 00:41:54 -0700

    Add the -manual flag to make handling missed incidents easier

--- tweetCollector.go
+++ tweetCollector.go
@@ -21,6 +21,76 @@ import (
 	"github.com/dghubble/oauth1"
 )
 
+// Global Variables
+var databasePath string
+var hashtags string
+var ministers string
+
+func processTweet(tweet *twitter.Tweet, twitterClient *twitter.Client, databaseConn *sql.DB) {
+	// Save information to the database here.
+	/*
+	 *	id (integer)
+	 * incidentid (int) = -1
+	 * tweetDate <tweet.created_at)
+	 * tweetID tweet.IDStr
+	 * tweetURL fmt.Printf("https://twitter.com/%s/status/%s\n", tweet.User.ScreenName, tweet.IDStr)
+	 * tweetText tweet.FullText
+	 */
+
+	tweetURL := fmt.Sprintf("https://twitter.com/yyctransport/status/%s", tweet.IDStr)
+	number := 0
+
+	var err error
+
+	// Some time math for interacting with SQLite from the Ruby Date in the Tweet object
+	tweetTime, err := time.Parse(time.RubyDate, tweet.CreatedAt)
+	if err != nil {
+		log.Fatal("Error parsing tweet time")
+	}
+	tweetDate := tweetTime.Format("2006-01-02")
+	tweetYear := tweetTime.Format("2006")
+
+	// Determine number of tweet in the year
+	query := fmt.Sprintf("select count(*)+1 from tweets where strftime('%%Y', tweetDate) = strftime('%%Y', '%s');", tweetDate)
+	err = databaseConn.QueryRow(query).Scan(&number)
+	if err != nil {
+		log.Fatal("Database error", err)
+	}
+
+	suffix := "th"
+	switch number % 10 {
+	case 1:
+		if (number % 100) != 11 {
+			suffix = "st"
+		}
+	case 2:
+		if (number % 100) != 12 {
+			suffix = "nd"
+		}
+	case 3:
+		if (number % 100) != 13 {
+			suffix = "rd"
+		}
+	}
+
+	tweetContent := fmt.Sprintf("%d%s tweeted pedestrian incident in %s.\n%s\n%s\n\n%s\n", number, suffix, tweetYear, hashtags, ministers, tweetURL)
+	tweet, resp, err := twitterClient.Statuses.Update(tweetContent, nil)
+
+	// Log Twitter's response
+	fmt.Println(tweetContent)
+	fmt.Println(resp)
+	fmt.Println(err)
+
+	sqlStmt := `
+		insert into tweets(tweetDate, tweetID, tweetURL, tweetText) values (?, ?, ?, ?)
+	`
+	// Need to convert tweet.CreatedAt to YYYY-MM-DD HH:MM:SS for storage in SQLite
+	databaseConn.Exec(sqlStmt, tweetTime.Format("2006-01-02 03:04:05"), tweet.IDStr, tweetURL, tweetContent)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
 func main() {
 
 	flags := flag.NewFlagSet("user-auth", flag.ExitOnError)
@@ -28,6 +98,9 @@ func main() {
 	consumerSecret := flags.String("consumer-secret", "", "Twitter Consumer Secret")
 	accessToken := flags.String("access-token", "", "Twitter Access Token")
 	accessSecret := flags.String("access-secret", "", "Twitter Access Secret")
+
+	manualTweet := flags.String("manual", "", "Manually enter a tweet to process")
+
 	databasePath := flags.String("database", "./peddown.db", "Database Path")
 	fhashtags := flags.String("hashtags", "", "Hashtags to include")
 	fministers := flags.String("ministers", "", "Transportation and Health Critics to include")
@@ -62,6 +135,24 @@ func main() {
 	// Twitter Client
 	client := twitter.NewClient(httpClient)
 
+	// Skip streaming if we're in manual mode
+	// ./peddown -manual <URL>
+	if *manualTweet != "" {
+		// Grab just the last part of the URL
+		tweetID, err := strconv.ParseInt(strings.Split(*manualTweet, "/")[len(strings.Split(*manualTweet, "/"))-1], 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		tweet, resp, err := client.Statuses.Show(tweetID, nil)
+
+		// Log Twitter's response
+		fmt.Println(resp)
+
+		processTweet(tweet, client, database.Conn)
+
+		os.Exit(0)
+	}
+
 	// Convenience Demux demultiplexed stream messages
 	// What to do with each type of tweet
 	demux := twitter.NewSwitchDemux()
@@ -77,63 +168,7 @@ func main() {
 			return
 		}
 
-		// Save information to the database here.
-		/*
-		*	id (integer)
-		* incidentid (int) = -1
-		* tweetDate <tweet.created_at)
-		* tweetID tweet.IDStr
-		* tweetURL fmt.Printf("https://twitter.com/%s/status/%s\n", tweet.User.ScreenName, tweet.IDStr)
-		* tweetText tweet.FullText
-		 */
-
-		tweetURL := fmt.Sprintf("https://twitter.com/yyctransport/status/%s", tweet.IDStr)
-		number := 0
-
-		err = database.Conn.QueryRow("select count(*)+1 from tweets where strftime('%Y', tweetDate) = strftime('%Y', 'now');").Scan(&number)
-		if err != nil {
-			log.Fatal("Database error", err)
-		}
-
-		suffix := "th"
-		switch number % 10 {
-		case 1:
-			if (number % 100) != 11 {
-				suffix = "st"
-			}
-		case 2:
-			if (number % 100) != 12 {
-				suffix = "nd"
-			}
-		case 3:
-			if (number % 100) != 13 {
-				suffix = "rd"
-			}
-		}
-
-		t := time.Now()
-		year := t.Format("2006")
-
-		tweetContent := fmt.Sprintf("%d%s tweeted pedestrian incident in %s.\n#yycwalk #yyccc #ableg #visionzero\n@RajanJSaw @shandro\n@lornedach @DShepYEG\n\n%s\n", number, suffix, year, tweetURL)
-		tweet, resp, err := client.Statuses.Update(tweetContent, nil)
-
-		// Log Twitter's response
-		fmt.Println(tweetContent)
-		fmt.Println(resp)
-		fmt.Println(err)
-
-		sqlStmt := `
-			insert into tweets(tweetDate, tweetID, tweetURL, tweetText) values (?, ?, ?, ?)
-		`
-		// Need to convert tweet.CreatedAt to YYYY-MM-DD HH:MM:SS for storage in SQLite
-		tweetTime, err := time.Parse(time.RubyDate, tweet.CreatedAt)
-		if err != nil {
-			log.Panic(err)
-		}
-		database.Conn.Exec(sqlStmt, tweetTime.Format("2006-01-02 03:04:05"), tweet.IDStr, tweetURL, tweetContent)
-		if err != nil {
-			log.Panic(err)
-		}
+		processTweet(tweet, client, database.Conn)
 	}
 
 	fmt.Println("Starting Stream...")
